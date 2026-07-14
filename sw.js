3fb0c5791265be65c03b7ecdeb8b93cda31ff85b
Leroy Evertse <leroy@hellofresh.nl>
2026-07-14 05:31:07 +0200

    Ergo-inset Fout: volgorde tekening naar 30 / 31 / 32, sw v29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'laagjes-v28';
+const CACHE = 'laagjes-v29';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg',
   './icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon-32.png'];
 self.addEventListener('install', e => {
