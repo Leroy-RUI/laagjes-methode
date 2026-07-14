546d891f51ee928dfe692d9e53538e70a73d7490
Leroy Evertse <leroy@hellofresh.nl>
2026-07-14 10:31:26 +0200

    DO NOT STACK-tape (groene band, volgt toppen box 25 -> box 3), sw v33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'laagjes-v32';
+const CACHE = 'laagjes-v33';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg',
   './icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon-32.png'];
 self.addEventListener('install', e => {
